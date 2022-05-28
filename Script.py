ccb1ecb031961e9d1a49cdf68cb07805d5ee30bb
akshayyt <106232049+akshayyt@users.noreply.github.com>
2022-05-28 14:30:50 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -4,18 +4,16 @@ class script(object):
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/Ak_Bot_SupportGroup>Team AkBotDevs</a>
+✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: Akshay Chand 🤓
 ✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
 ✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
 ✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
-✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
+✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v3.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     ADMINS_TXT = """Hey Bruh,
-My name is LUCIFER
+My name is Aladdin
 - My Admins are:
-- @IAM_A_JOKER
-  @JINN_SULTHAN
-  @psychopath_here 
+- 
 
 <b>DEVS:</b>
 - <a href=https://t.me/Ak_Bot_SupportGroup>Team AkBotDevs</a>"""
@@ -24,7 +22,7 @@ My name is LUCIFER
 - Filter is the feature were users can set automated replies for a particular keyword and LUCIFER will respond whenever a keyword is found the message
 
 <b>NOTE:</b>
-1. LUCIFER should have admin privillage.
+1. Aladdin should have admin privillage.
 2. only admins can add filters in a chat.
 3. alert buttons have a limit of 64 characters.
 
@@ -43,7 +41,7 @@ My name is LUCIFER
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/Ls_Supportz)</code>
+<code>[Button Text](buttonurl:http://t.me/AladdinFilter_Bot)</code>
 
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
